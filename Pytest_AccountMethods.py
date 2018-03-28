1e4dafb9e794117b79a374b261262d39dd3fe461
Smira87 <snoff87@gmail.com>
2018-03-28 06:06:57 +0300

    Update Pytest_AccountMethods.py

--- Pytest_AccountMethods.py
+++ Pytest_AccountMethods.py
@@ -4,7 +4,7 @@ from helpers import checkResponseOK, compareDictKeyValues, checkLog
 from fixtures import setupRoutingGroup, teardownRoutingGroup
 
 
-class Pyest_AccountMethods:
+class Pytest_AccountMethods:
 
 
     @classmethod
